043d93b1f0c2aee9b365c0b34d06e51a4115fee7
pujunjie <13404213324@163.com>
2020-12-27 20:50:53 +0800

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const { program } = require("commander");
+const craeteHelpOptions = require("./lib/core/help");
+const createCommands = require("./lib/core/create");
+
+// 查看版本
+program.version(require("./package.json").version);
+
+// 帮助和可选信息
+craeteHelpOptions();
+
+// 创建其他指令
+createCommands();
+
+// 解析终端指令
+program.parse(process.argv);
